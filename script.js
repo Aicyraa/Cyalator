1f5f943f73fd934dad9e8430183083f628313239
Aicyraa <182109647+Aicyraa@users.noreply.github.com>
2025-12-21 19:11:06 +0800

    completed html structure and added a little layouting :>>

---
+++ script.js
@@ -0,0 +1,6 @@
+// has a function "operate" that takes an expression??? or a 3 value FORMAT: * operand,operator,operand
+// If we pressed on a operator, it should process the 2 number first (if available) and display and save the result before continuing
+// MUST!
+   // Displays an error if 1 operand is the only input
+   // Can only input 1 decimal, Disable after pressing for the first time 
+
