4f6b283d38f1bfa8432e94f5e45cc713dde93d02
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2025-12-22 21:18:54 +0800

    working function SetDisplay removeDisplay removeCharacter

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-// has a function "operate" that takes an expression??? or a 3 value FORMAT: * operand,operator,operand
-// If we pressed on a operator, it should process the 2 number first (if available) and display and save the result before continuing
-// MUST!
-   // Displays an error if 1 operand is the only input
-   // Can only input 1 decimal, Disable after pressing for the first time 
+/* 
+   Parts:
+   1. Make a object containing the keys, numbers, operators
+   2. function for displaying the corresponding numbers and operators
+   3. function for processing / evaluating the input from display
+      * must prevent from inputing 2 decimal or dot ** Disabling the button
+      * display error if 1 operand is provided
+      * should only compute / evaluate 2 number at a time then save it to a result variable
+*/
 
+const numKeys = document.querySelector(".container-numkeys");
+const display = document.querySelector("#input");
+const del = document.querySelector("#del");
+const clear = document.querySelector("#clear");
+
+(function () {
+   removeDisplay();
+   numKeys.addEventListener("click", setDisplay);
+   del.addEventListener("click", removeDisplay);
+   clear.addEventListener("click", removeChar);
+})();
+
+function setDisplay(e) {
+   const value = e.target.id;
+   if (value === undefined || ["clear", "del", "compute"].includes(value)) return
+   display.value += value;
+}
+
+function removeDisplay() {
+   display.value = "";
+}
+
+function removeChar() {
+   const value = display.value
+   display.value = value.slice(0, value.length - 1)
+}
